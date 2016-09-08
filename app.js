a392f0c48f56a6df9688d19395f2764936cae427
Jeff <jeffluo35@gmail.com>
2016-09-08 05:40:10 +0000

    Add client commands and help, make things less confusing also add admin commands

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var Entities = require('html-entities').AllHtmlEntities;
 var entities = new Entities();
 
 const PORT = process.env.PORT || 3000;
+const operpass = process.env.OPERPASS || 'passwd';
 
 var app = express();
 app.use(express.static(path.join(__dirname, '/static')));
@@ -66,14 +67,15 @@ function sendTo(socket, msg) {
 io.on('connection', function(socket) {
 	var cnum = availablecnum.pop();
 	if (cnum == undefined) {
-		var cnum = clientnum;
+		cnum = clientnum;
 		clientnum++;
 	}
 	log.info('Connection from '+socket.handshake.address+' with id '+cnum);
 	socket.clientid = cnum;
 	clients[cnum] = {
 		socket: socket,
-		nick: "client"+socket.clientid
+		nick: "client"+socket.clientid,
+		admin: false
 	};
 	nicks[clients[socket.clientid].nick] = socket;
 	sendToAll('* '+clients[socket.clientid].nick+' has joined the room');
@@ -92,7 +94,11 @@ io.on('connection', function(socket) {
 				case "/nick":
 					if (nicks[msg[1]]) {
 						sendTo(this, '* '+msg[1]+': Nick is already in use');
-					} else {	
+					} else if (msg[1] == undefined) {
+						sendTo(this, '* Please supply a nick');
+					} else if (msg[1].length >= 25) {
+						sendTo(this, '* '+msg[1]+': Nick is too long');
+					} else {
 						sendToAll('* '+clients[this.clientid].nick+' has changed their nick to '+msg[1]);
 						delete(nicks[clients[this.clientid].nick]);
 						nicks[msg[1]] = this;
@@ -101,10 +107,79 @@ io.on('connection', function(socket) {
 					break;
 				case "/me":
 					msg.splice(0,1);
-					sendToAll('* '+clients[this.clientid].nick+' '+msg.join(' '));
+					sendToAll('- '+clients[this.clientid].nick+' '+msg.join(' '));
+					break;
+				case "/oper":
+					if(msg[1]==operpass){
+						sendTo(this, "* We would like to take this moment to remind you that we accept absolutely no liability for the insanity you're about to endure.");
+						log.notice(clients[this.clientid].nick+' has opered from '+this.handshake.address+' with id '+this.clientid);
+						clients[this.clientid].admin = true;
+					} else {
+						sendTo(this, '* Oper failed. This incident was logged.');
+						log.notice('Failed oper from '+this.handshake.address+' with id '+this.clientid);
+					}
+					break;
+				case "/kill":
+					if(clients[this.clientid].admin){
+						sendTo(nicks[msg[1]], '* You have been killed by an admin');
+						nicks[msg[1]].disconnect();
+						log.notice(clients[this.clientid].nick+' has killed '+msg[1]);
+					} else {
+						sendTo(this, "* I'm sorry, Dave. I'm afraid I can't do that.");
+					}
+					break;
+				case "/eval":
+					if(clients[this.clientid].admin){
+						msg.splice(0,1);
+						try{
+							sendTo(this, '* Result: '+eval(msg.join(' ')).toString());
+						} catch (e) {
+							sendTo(this, '* Error: '+e.toString());
+						}
+					} else {
+						sendTo(this, "* I'm sorry, Dave. I'm afraid I can't do that.");
+					}
 					break;
 				case "/list":
 					sendTo(this, '* List of currently connected clients: '+Object.keys(nicks).join(' '));
+					break;
+				case "/help":
+					if (msg[1] == undefined) {
+						msg[1] = ' ';
+					}
+					switch(msg[1].toLowerCase()) {
+						case 'nick':
+							sendTo(this, '* nick <newnick>: Changes your nickname');
+							break;
+						case 'me':
+							sendTo(this, '* me <action>: Sends an action in 3rd person');
+							break;
+						case 'list':
+							sendTo(this, '* list: Lists all online users');
+							break;
+						/* Don't list this, it is a bad idea
+						case 'oper':
+							sendTo(this, '* oper: Gives you crap ammounts of power')
+							break; */
+						case 'kill':
+							if (clients[this.clientid].admin) {
+								sendTo(this, '* kill: Forcibly disconnects a client');
+								break;
+							}
+						case 'eval':
+							if (clients[this.clientid].admin) {
+								sendTo(this, '* eval: Runs javascript code on the server');
+								break;
+							}
+						case ' ':
+							if (clients[this.clientid].admin) {
+								sendTo(this, '* Help topics available to opers: kill eval');
+							}
+							sendTo(this, '* Available help topics: nick me list');
+							break;
+						default:
+							sendTo(this, '* '+msg[1]+': No such help topic');
+					}
 					break;
 				default:
 					sendTo(this, "* "+msg[0]+": Command not found");
